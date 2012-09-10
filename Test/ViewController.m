ea560ed2d73886f22e5fc6ff7ef438777dd6409a
Card <card_reevs@hotmail.com>
2012-09-10 14:51:54 +0700

    got it

--- Test/ViewController.m
+++ Test/ViewController.m
@@ -24,6 +24,7 @@
 {
     [super viewDidUnload];
     // Release any retained subviews of the main view.
+    //got it
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
