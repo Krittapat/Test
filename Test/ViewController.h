e778a17e3ab6e1ff9a2bbb58e91e9e5811147093
Card <card_reevs@hotmail.com>
2012-09-10 14:45:21 +0700

    Initial Commit

---
+++ Test/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by Card on 9/10/12.
+//  Copyright (c) 2012 Card. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ TestTests/TestTests.h
@@ -0,0 +1,13 @@
+//
+//  TestTests.h
+//  TestTests
+//
+//  Created by Card on 9/10/12.
+//  Copyright (c) 2012 Card. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TestTests : SenTestCase
+
+@end
